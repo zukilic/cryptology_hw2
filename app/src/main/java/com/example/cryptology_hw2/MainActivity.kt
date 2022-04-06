a53c36bf3d3941bf4820eb35b7271e3da8781249
zukilic <55054309+zukilic@users.noreply.github.com>
2022-04-07 00:40:55 +0300

    3

--- app/src/main/java/com/example/cryptology_hw2/MainActivity.kt
+++ app/src/main/java/com/example/cryptology_hw2/MainActivity.kt
@@ -13,8 +13,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var signin : Button
     lateinit var login : Button
 
-    var itis = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
